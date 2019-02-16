efc259ee45db1db2ef73d0d654005cfe7ef22789
Akinjide Bankole <r@akinjide.me>
2019-02-17 00:56:01 +0100

    refactor: remove unused vendor css and compress used css

---
+++ client/src/styles/vendor.js
@@ -0,0 +1,7 @@
+var lib_base = 'client/public/lib/';
+
+module.exports = [
+  lib_base + 'lumx/dist/lumx.css',
+  lib_base + 'trix/dist/trix.css',
+  lib_base + 'angular-loading-bar/build/loading-bar.min.css'
+];
